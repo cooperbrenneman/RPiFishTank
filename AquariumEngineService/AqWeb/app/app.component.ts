eaa0bc6211f7f0f7b7304af49ad762fa82982576
Kelly Smith <ksmith@innobdesign.com>
2016-08-13 12:41:46 -0700

    Initial angular 2 bootstraping

---
+++ AquariumEngineService/AqWeb/app/app.component.ts
@@ -0,0 +1,34 @@
+﻿import { Component } from '@angular/core';
+import { Hero } from './hero/hero';
+
+
+const HEROES: Hero[] = [
+    { id: 11, name: 'Mr. Nice' },
+    { id: 12, name: 'Narco' },
+    { id: 13, name: 'Bombasto' },
+    { id: 14, name: 'Celeritas' },
+    { id: 15, name: 'Magneta' },
+    { id: 16, name: 'RubberMan' },
+    { id: 17, name: 'Dynama' },
+    { id: 18, name: 'Dr IQ' },
+    { id: 19, name: 'Magma' },
+    { id: 20, name: 'Tornado' }
+];
+
+@Component({
+    selector: 'my-app',
+    templateUrl: 'app/app.component.html',
+    styleUrls: ['app/app.component.css']
+})
+export class AppComponent {
+    public heroes = HEROES;
+
+
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+
+
+    onSelect(hero: Hero) {
+        this.selectedHero = hero;
+    }
+}
